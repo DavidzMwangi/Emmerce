db06659cdd5763f9bf00efbed93d3a1355b4a3eb
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2025-01-04 17:42:41 +0300

    Addition of token and working authentication of the FE

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from .views import LoginView
 
 router = DefaultRouter()
 router.register(r'leads', views.LeadViewSet, basename='lead')
@@ -9,5 +10,7 @@ router.register(r'notes', views.NoteViewSet, basename='note')
 router.register(r'reminders', views.ReminderViewSet, basename='reminder')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+    path('auth/login', LoginView.as_view(), name='login'),
+
 ]
--- api/views.py
+++ api/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
+
+
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from rest_framework.authtoken.models import Token
+from rest_framework import status
 
 from api.models import Lead, Contact, Reminder, Note
 from api.serializers import LeadSerializer, ContactSerializer, NoteSerializer, ReminderSerializer
@@ -56,3 +62,37 @@ class ReminderViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        if not email or not password:
+            return Response({'error': 'Email and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Authenticate the user using email and password
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({'error': 'Invalid email or password.'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        user = authenticate(username=user.username, password=password)
+        if not user:
+            return Response({'error': 'Invalid email or password.'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        # Generate or retrieve the token
+        token, _ = Token.objects.get_or_create(user=user)
+
+        # Return the user data and token
+        return Response({
+            'data': {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'token': token.key
+            }
+        })
