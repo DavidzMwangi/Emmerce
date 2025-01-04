6c97edfe2b1c0c669dd18a6baefb6514eaddd9d6
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2025-01-04 12:07:00 +0300

    Addition of models to the admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from api.models import Lead, Contact, Note, Reminder
+
 # Register your models here.
+admin.site.register(Lead)
+admin.site.register(Contact)
+admin.site.register(Note)
+admin.site.register(Reminder)
